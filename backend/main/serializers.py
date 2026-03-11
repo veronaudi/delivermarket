0685b96b5cadbbb1fb5b14f41820798dace8dadc
veron <veronikasegoleva4456@gmail.com>
2026-03-11 21:07:04 +1000

    ster

--- backend/main/serializers.py
+++ backend/main/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import UserProfile, Inventory
 from django.contrib.auth.password_validation import validate_password
+
+from .models import UserProfile, Category, Dish
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -10,37 +11,38 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
+        fields = ("username", "password", "password2", "email", "first_name", "last_name")
         extra_kwargs = {
-            'first_name': {'required': False},
-            'last_name': {'required': False},
-            'email': {'required': False}
+            "first_name": {"required": False},
+            "last_name": {"required": False},
+            "email": {"required": False},
         }
 
     def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError({"password": "Passwords didn't match"})
         return attrs
 
     def create(self, validated_data):
-        validated_data.pop('password2')
-        
+        validated_data.pop("password2")
+
         user = User.objects.create(
-            username=validated_data['username'],
-            email=validated_data.get('email', ''),
-            first_name=validated_data.get('first_name', ''),
-            last_name=validated_data.get('last_name', '')
+            username=validated_data["username"],
+            email=validated_data.get("email", ""),
+            first_name=validated_data.get("first_name", ""),
+            last_name=validated_data.get("last_name", ""),
         )
-        
-        user.set_password(validated_data['password'])
+
+        user.set_password(validated_data["password"])
         user.save()
-        
+
         return user
-    
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ["id", "username", "email"]
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
@@ -48,4 +50,18 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserProfile
-        fields = ['id', 'user', 'created_at']
+        fields = ["id", "user", "phone", "address", "created_at"]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
+
+
+class DishSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+
+    class Meta:
+        model = Dish
+        fields = ["id", "name", "category", "description", "price", "image_url"]
